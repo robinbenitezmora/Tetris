315afe47b3d8e597e2feae9bc43da76dec9fd67b
robinbenitezmora <robinbenitez1990@gmail.com>
2023-12-18 20:39:18 -0500

    Add more code

--- Tetris.java
+++ Tetris.java
@@ -305,32 +305,32 @@ public class Tetris extends JFrame implements KeyListener {
     }
 
     private void blockgen() {
-            Component temporaryLostComponent = null;
-            pos[0] = 0;
-            @Override
-            void go() {
-                // Add the implementation of the go() method here
-            }
+        Component temporaryLostComponent = null;
+        pos[0] = 0;
+        pos[1] = 1;
+        rand = (int) (Math.random() * 7) + 1;
+        centralx = 4;
+        centraly = 0;
 
-            int rowsclrd = 0; // Declare and initialize the variable rowsclrd
-            centraly = 0;
-            System.out.print(rand);
-            if ((b[4+prof[pos[0]][0][rand-1].x][prof[pos[0]][0][rand-1].y].getBackground() == Color.DARK_GRAY) &&
-                (b[4+prof[pos[0]][1][rand-1].x][prof[pos[0]][1][rand-1].y].getBackground() == Color.DARK_GRAY) &&
-                (b[4+prof[pos[0]][2][rand-1].x][prof[pos[0]][2][rand-1].y].getBackground() == Color.DARK_GRAY) &&
-                (b[4+prof[pos[0]][3][rand-1].x][prof[pos[0]][3][rand-1].y].getBackground() == Color.DARK_GRAY)) {
-                b[4+prof[pos[0]][0][rand-1].x][prof[pos[0]][0][rand-1].y].setBackground(rnd);
-                b[4+prof[pos[0]][1][rand-1].x][prof[pos[0]][1][rand-1].y].setBackground(rnd);
-                b[4+prof[pos[0]][2][rand-1].x][prof[pos[0]][2][rand-1].y].setBackground(rnd);
-                b[4+prof[pos[0]][3][rand-1].x][prof[pos[0]][3][rand-1].y].setBackground(rnd);
-                go();
-            } else {
-                JOptionPane.showMessageDialog(null, "Game Over! You cleared "+rowsclrd+" rows, well done!");
-                System.exit(0);
-            }
+        System.out.print(rand);
+
+        int rowsclrd = 0; // Declare and initialize the variable rowsclrd
+        centraly = 0;
+        System.out.print(rand);
+        if ((b[4 + prof[pos[0]][0][rand - 1].x][prof[pos[0]][0][rand - 1].y].getBackground() == Color.DARK_GRAY) &&
+                (b[4 + prof[pos[0]][1][rand - 1].x][prof[pos[0]][1][rand - 1].y].getBackground() == Color.DARK_GRAY) &&
+                (b[4 + prof[pos[0]][2][rand - 1].x][prof[pos[0]][2][rand - 1].y].getBackground() == Color.DARK_GRAY) &&
+                (b[4 + prof[pos[0]][3][rand - 1].x][prof[pos[0]][3][rand - 1].y].getBackground() == Color.DARK_GRAY)) {
+            b[4 + prof[pos[0]][0][rand - 1].x][prof[pos[0]][0][rand - 1].y].setBackground(rnd);
+            b[4 + prof[pos[0]][1][rand - 1].x][prof[pos[0]][1][rand - 1].y].setBackground(rnd);
+            b[4 + prof[pos[0]][2][rand - 1].x][prof[pos[0]][2][rand - 1].y].setBackground(rnd);
+            b[4 + prof[pos[0]][3][rand - 1].x][prof[pos[0]][3][rand - 1].y].setBackground(rnd);
+            go();
+        } else {
+            JOptionPane.showMessageDialog(temporaryLostComponent,
+                    "Game Over! You cleared " + rowsclrd + " rows, well done!");
+            System.exit(0);
         }
-
-    private void go() {
     }
 
     public void rotate() {
@@ -382,4 +382,181 @@ public class Tetris extends JFrame implements KeyListener {
         }
     }
 
-}
+    public int getxs() {
+        int xs = 0;
+        int[] xf = {-1, -1, -1, -1};
+        for (int d = 0; d < 4; d++) {
+            if (xf[0] != prof[pos[0]][d][rand - 1].x) || (xf[1] != prof[pos[0]][d][rand-1].x) || (xf[2] != prof[pos[0]][d][rand-1].x) || (xf[3] != prof[pos[0]][d][rand-1].x)){
+                xf[d] = prof[pos[0]][d][rand-1].x;
+            }
+        }
+        for (int d = 0; d < 4; d++) {
+            if (xf[d] != -1) {
+                xs++;
+            }
+        }
+        return xs;
+    }
+
+    public void movedown() {
+        int[] m2 = { -1, -1, -1, -1 };
+        int[] m1 = { -1, -1, -1, -1 };
+        int[] zero = { -1, -1, -1, -1 };
+        int[] one = { -1, -1, -1, -1 };
+        int[] two = { -1, -1, -1, -1 };
+
+        for (int d = 0; d < 4; d++) {
+            if (prof[pos[0]][d][rand - 1].x == -2) {
+                m2[d] = d;
+            } else if (prof[pos[0]][d][rand - 1].x == -1) {
+                m1[d] = d;
+            } else if (prof[pos[0]][d][rand - 1].x == 0) {
+                zero[d] = d;
+            } else if (prof[pos[0]][d][rand - 1].x == 1) {
+                one[d] = d;
+            } else if (prof[pos[0]][d][rand - 1].x == 2) {
+                two[d] = d;
+            }
+        }
+        int tmpm2 = -5;
+        int tmpm1 = -5;
+        int tmpzero = -5;
+        int tmpone = -5;
+        int tmptwo = -5;
+        for (int d = 0; d < 4; d++) {
+            if (m2[d] != -1) {
+                if (tmpm2 < prof[pos[0]][md[d]][rand - 1].y) {
+                    tmpm2 = prof[pos[0]][md[d]][rand - 1].y;
+                }
+            }
+            if (m1[d] != -1) {
+                if (tmpm1 < prof[pos[0]][md[d]][rand - 1].y) {
+                    tmpm1 = prof[pos[0]][md[d]][rand - 1].y;
+                }
+            }
+            if (zero[d] != -1) {
+                if (tmpzero < prof[pos[0]][md[d]][rand - 1].y) {
+                    tmpzero = prof[pos[0]][md[d]][rand - 1].y;
+                }
+            }
+            if (one[d] != -1) {
+                if (tmpone < prof[pos[0]][md[d]][rand - 1].y) {
+                    tmpone = prof[pos[0]][md[d]][rand - 1].y;
+                }
+            }
+            if (two[d] != -1) {
+                if (tmptwo < prof[pos[0]][md[d]][rand - 1].y) {
+                    tmptwo = prof[pos[0]][md[d]][rand - 1].y;
+                }
+            }
+        }
+        int total = 0;
+        for (int d = 0; d < 4; d++) {
+            if (prof[pos[0]][d][rand - 1].x == -2) {
+                if (perim[2 + centralx + -2][2 + centraly + tmpm2 + 1] != 4) {
+                    if (b[centralx + -2][centraly + tmpm2 + 1].getBackground() == Color.DARKGRAY) {
+                        total++;
+                    }
+                }
+            } else if (prof[pos[0]][d][rand - 1].x == -1) {
+                if (perim[2 + centralx + -1][2 + centraly + tmpm1 + 1] != 4) {
+                    if (b[centralx + -1][centraly + tmpm1 + 1].getBackground() == Color.DARK_GRAY) {
+                        total++;
+                    }
+                }
+            } else if (prof[pos[0]][d][rand - 1].x == 0) {
+                if (perim[2 + centralx + 0][2 + centraly + tmpzero + 1] != 4) {
+                    if (b[centralx + 0][centraly + tmpzero + 1].getBackground() == Color.DARK_GRAY) {
+                        total++;
+                    }
+                }
+            } else if (prof[pos[0]][d][rand - 1].x == 1) {
+                if (perim[2 + centralx + 1][2 + centraly + tmpone + 1] != 4) {
+                    if (b[centralx + 1][centraly + tmpone + 1].getBackground() == Color.DARK_GRAY) {
+                        total++;
+                    }
+                }
+            } else if (prof[pos[0]][d][rand - 1].x == 2) {
+                if (perim[2 + centralx + 2][2 + centraly + tmptwo + 1] != 4) {
+                    if (b[centralx + 2][centraly + tmptwo + 1].getBackground() == Color.DARK_GRAY) {
+                        total++;
+                    }
+                }
+            } else if (prof[pos[0]][d][rand - 1].x == 2) {
+                if (perim[2 + centralx + 2][2 + centraly + tmptwo + 1] != 4) {
+                    if (b[centralx + 2][centraly + tmptwo + 1].getBackground() == Color.DARK_GRAY) {
+                        total++;
+                    }
+                }
+            }
+        }
+        if (total == 4) {
+            centraly++;
+            b[centralx + prof[pos[0]][0][rand - 1].x][centraly + prof[pos[0]][0][rand - 1].y]
+                    .setBackground(Color.DARK_GRAY);
+            b[centralx + prof[pos[0]][1][rand - 1].x][centraly + prof[pos[0]][1][rand - 1].y]
+                    .setBackground(Color.DARK_GRAY);
+            b[centralx + prof[pos[0]][2][rand - 1].x][centraly + prof[pos[0]][2][rand - 1].y]
+                    .setBackground(Color.DARK_GRAY);
+            b[centralx + prof[pos[0]][3][rand - 1].x][centraly + prof[pos[0]][3][rand - 1].y]
+                    .setBackground(Color.DARK_GRAY);
+            b[centralx + prof[pos[0]][0][rand - 1].x][centraly + prof[pos[0]][0][rand - 1].y]
+                    .setBackground(rnd[rand - 1]);
+            b[centralx + prof[pos[0]][1][rand - 1].x][centraly + prof[pos[0]][1][rand - 1].y]
+                    .setBackground(rnd[rand - 1]);
+            b[centralx + prof[pos[0]][2][rand - 1].x][centraly + prof[pos[0]][2][rand - 1].y]
+                    .setBackground(rnd[rand - 1]);
+            b[centralx + prof[pos[0]][3][rand - 1].x][centraly + prof[pos[0]][3][rand - 1].y]
+                    .setBackground(rnd[rand - 1]);
+        } else {
+            bottom = true;
+        }
+    }
+
+    public void go() {
+        do {
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            movedown();
+            rowcheck();
+        } while (bottom == false);
+        bottom = false;
+        blockgen();
+    }
+
+    public void rowcheck() {
+        int row = 0;
+        for (int y = 0; y < 20; y++) {
+            for (int x = 0; x < 10; x++) {
+                if (b[x][y].getBackground() != Color.DARK_GRAY) {
+                    row++;
+                }
+                if (row == 10) {
+                    rowscird++;
+                    rowclear(y);
+                }
+            }
+            row = 0;
+        }
+    }
+
+    public void rowclear(int y){
+        int inc = 0;
+        for (int x = 0; x < 10; x++) {
+            b[x][y].setBackground(Color.DARK_GRAY);
+        }
+        for (int c = y - 1; c > -1; c--) {
+            for (int x = 0; x < 10; x++) {
+                tmp[x][y - inc] = Color.rgb(b[x][c].getBackground().getRed(), b[x][c].getBackground().getGreen(), b[x][c].getBackground().getBlue());
+            }
+            inc++;
+        }
+        for (int c = y; c > -1; c--) {
+            for (int x = 0; x < 10; x++) {
+                b[x][c].setBackground(tmp[x][c]);
+            }
+        }
+    }
